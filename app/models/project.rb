de4254ff8a06d3f0da999f3d563732a49f7285d7
Michael Sprauer <Sprauer@Inline.de>
2015-03-25 18:25:25 +0100

    Fixed breadcrumbs

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,6 @@
 class Project < ActiveRecord::Base
   has_and_belongs_to_many :categories
 
-  after_initialize :load_details
-
   def load_details
     LoadProjectDetails.perform_later self if downloads.blank?
   end
